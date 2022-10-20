c23623f6794590dbd827c0cfdac628cd56062fb6
ambra-dipiano <ambra.dipiano@inaf.it>
2022-10-20 16:06:56 +0200

    add __init__.py

---
+++ tutorials/__init__.py
@@ -0,0 +1,9 @@
+# *****************************************************************************
+# Copyright (C) 2022 INAF
+# This software is distributed under the terms of the BSD-3-Clause license
+#
+# This software is intended to install the library
+# *****************************************************************************
+
+# Import sub modules
+from . import *
